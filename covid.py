801a504aa65635d66f358e240e559633f5dd1054
Stephen Tondale <37376169+Stephen2206@users.noreply.github.com>
2020-05-16 21:34:10 +0530

    Update covid.py

--- covid.py
+++ covid.py
@@ -91,7 +91,7 @@ future_forecast = np.ceil(future_forecast)
 zipped_forecast = list(zip(future, future_forecast))
 df_future = pd.DataFrame(zipped_forecast, columns=['Date', 'Cases'])
 
-observed_yesterday = daily_cases[-1]['totalconfirmed']
+observed_yesterday = daily_cases[-2]['totalconfirmed']
 expected = int(future_forecast[0]) - int(observed_yesterday)
 
 # Graphs depicting Daily Changes
